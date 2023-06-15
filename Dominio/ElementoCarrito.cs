e12b5ab81d1454b4107ddfb27a0961c85ebcbd90
Marcos <marcosluciano000@gmail.com>
2023-06-14 22:06:52 -0300

    Creación de las clases faltantes

---
+++ Dominio/ElementoCarrito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ElementoCarrito
+    {
+        public int IDElemento { get; set; }
+        public Producto producto { get; set; }
+        public int cantidad { get; set; }
+    }
+}
---
+++ Negocio/TipoUsuarioNegocio.cs
@@ -0,0 +1,19 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class TipoUsuarioNegocio
+    {
+        TipoUsuario tipoUsuario;
+
+        TipoUsuarioNegocio()
+        {
+            tipoUsuario = new TipoUsuario();
+        }
+    }
+}
